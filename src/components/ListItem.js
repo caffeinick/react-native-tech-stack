583f3340ee45614cc4cb55df866f13297b80be2e
Juman Pak <qkrwnaks92@gmail.com>
2018-03-15 14:32:44 +0900

    100. Expanding a Row

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,6 +7,16 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+    
+    if(library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
@@ -21,6 +31,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     )
@@ -29,7 +40,8 @@ class ListItem extends Component {
 
 ListItem.propTypes = {
   library : PropTypes.object,
-  selectLibrary: PropTypes.func
+  selectLibrary: PropTypes.func,
+  selectedLibraryId: PropTypes.number
 }
 
 const styles = {
@@ -39,4 +51,8 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateTopProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+}
+
+export default connect(mapStateTopProps, actions)(ListItem);
